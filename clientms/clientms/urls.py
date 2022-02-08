c9e7d55d87c882fa35fa05227e9762fe0ef0db81
urana94 <urana94@gmail.com>
2022-02-08 08:16:22 -0600

    3

--- clientms/clientms/urls.py
+++ clientms/clientms/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
+    path('clients/', include('clients.urls')),
     path('users/', include('django.contrib.auth.urls')),
 ]
 
